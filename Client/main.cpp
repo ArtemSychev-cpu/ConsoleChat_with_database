b52a5a3cccb46b8cd6b4d1cb7a78af4dbc2988b9
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-08-16 16:37:33 +0300

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,1 +1,15 @@
+#include "Client.h"
+#include "Chat.h"
 
+int main() {
+
+	Sleep(3000);
+
+	std::string ip{ "127.0.0.1" };
+	uint16_t port{ 5555 };
+
+	Chat chat(ip, port);
+	chat.run();
+
+	return 0;
+}
